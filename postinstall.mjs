d403c8cb841303829698f7b919890c86d77dc86f
astnerdavid <158482980+astnerdavid@users.noreply.github.com>
2025-03-06 18:53:22 +0100

    fix: config deployment on installation

--- postinstall.mjs
+++ postinstall.mjs
@@ -1,17 +1,19 @@
-// biome-ignore lint/correctness/noNodejsModules: we need node imports here
+// biome-ignore lint/correctness/noNodejsModules: we need the node module here
 import {existsSync, writeFileSync} from 'node:fs';
-// biome-ignore lint/correctness/noNodejsModules: we need node imports here
+// biome-ignore lint/correctness/noNodejsModules: we need the node module here
 import {resolve} from 'node:path';
 
+const rootPath = resolve(__dirname, '..');
+
 const files = {
-	'biome.jsonc': '{ "extends": "./node_modules/tivid-code-quality/biome-config.json" }',
+	'biome.json': '{ "extends": "./node_modules/tivid-code-quality/biome-config.json" }',
 	'.prettierrc.json': '{ "extends": "./node_modules/tivid-code-quality/prettier-config.json" }'
 };
 
 for (const [fileName, content] of Object.entries(files)) {
-	const filePath = resolve(process.cwd(), fileName);
+	const filePath = resolve(rootPath, fileName);
 
 	if (existsSync(filePath)) {
-		writeFileSync(filePath, content);
+		writeFileSync(filePath, content); // writes the config files into the root of the project
 	}
 }
