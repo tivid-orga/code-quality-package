65719a06e1bcdcd7ba22ec09e2b83d6b248cc38c
astnerdavid <158482980+astnerdavid@users.noreply.github.com>
2025-03-06 19:09:59 +0100

    feat: remove comments since biome is not linting anymore

--- postinstall.mjs
+++ postinstall.mjs
@@ -1,15 +1,14 @@
-// biome-ignore lint/correctness/noNodejsModules: we need the node module here
-import {existsSync, writeFileSync} from 'node:fs';
-// biome-ignore lint/correctness/noNodejsModules: we need the node module here
-import {resolve} from 'node:path';
-// biome-ignore lint/correctness/noNodejsModules: we need the node module here
-import {fileURLToPath} from 'node:url';
+import { existsSync, writeFileSync } from "node:fs";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
-const Dirname = fileURLToPath(new URL('.', import.meta.url));
-const rootPath = resolve(Dirname, '../..');
+const Dirname = fileURLToPath(new URL(".", import.meta.url));
+const rootPath = resolve(Dirname, "../..");
 const files = {
-	'biome.json': '{ "extends": "./node_modules/tivid-code-quality/biome-config.jsonc" }',
-	'.prettierrc.json': '{ "extends": "./node_modules/tivid-code-quality/prettier-config.jsonc" }'
+	"biome.json":
+		'{ "extends": "./node_modules/tivid-code-quality/biome-config.jsonc" }',
+	".prettierrc.json":
+		'{ "extends": "./node_modules/tivid-code-quality/prettier-config.jsonc" }',
 };
 
 for (const [fileName, content] of Object.entries(files)) {
