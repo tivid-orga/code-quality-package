5a13efc98fba3ad208c81b029d86439b55ecdad2
astnerdavid <158482980+astnerdavid@users.noreply.github.com>
2025-03-06 19:40:15 +0100

    feat: ignore prettier-config in biome and fix prettier import

--- postinstall.mjs
+++ postinstall.mjs
@@ -5,10 +5,11 @@ import { fileURLToPath } from "node:url";
 const Dirname = fileURLToPath(new URL(".", import.meta.url));
 const rootPath = resolve(Dirname, "../..");
 const files = {
-	"biome.json":
-		'{"extends":["./node_modules/tivid-code-quality/biome-config.jsonc"]}',
-	".prettierrc.mjs":
-		"export {default} from 'tivid-code-quality/prettier-config.json';",
+	"biome.json": `{
+		"$schema": "https://biomejs.dev/schemas/1.9.4/schema.json",
+		"extends": ["./node_modules/tivid-code-quality/biome-config.jsonc"]
+	}`,
+	".prettierrc.mjs": `export {default} from 'tivid-code-quality/prettier-config.json';`,
 };
 
 for (const [fileName, content] of Object.entries(files)) {
